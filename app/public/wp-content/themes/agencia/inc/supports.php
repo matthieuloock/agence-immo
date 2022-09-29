d14370cf4eaf25426a1a0f17f7b6e167a65f1ee2
Matthieu LOOCK <m01ck@proton.me>
2022-09-29 17:57:06 +0200

    svg support added

--- app/public/wp-content/themes/agencia/inc/supports.php
+++ app/public/wp-content/themes/agencia/inc/supports.php
@@ -10,3 +10,8 @@ add_action('after_setup_theme', function(){
     add_theme_support('html5');
     add_theme_support('post-thumbnails');
 });
+
+add_filter('upload_mimes', function ($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+});
