4e58f4641c98ff0829b88e604d82a9414fdad5cf
Matthieu LOOCK <m01ck@proton.me>
2022-11-29 17:08:22 +0100

    svg support added

--- app/public/wp-content/themes/agencia/inc/assets.php
+++ app/public/wp-content/themes/agencia/inc/assets.php
@@ -3,4 +3,5 @@ add_action('wp_enqueue_scripts', function(){
     // Ici on ajoute les scripts CSS et JS qui étaient dans le HEAD
     wp_enqueue_style('main', get_template_directory_uri() . '/assets/style.e2e1a33c.css');
     wp_enqueue_script('main', get_template_directory_uri() . '/assets/main.ef23f591.js');
+    
 });
